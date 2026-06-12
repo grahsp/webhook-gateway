d7ae95a32acb16596ef85c0a81744c687e5b3e6e
grahsp <pontus.grahsp@gmail.com>
2026-06-12 11:29:46 +0200

    chore: add new migrations

---
+++ backend/WebhookGateway.API/Persistence/Migrations/20260612085256_WebhookRoutes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebhookGateway.API.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WebhookRoutes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_WebhookEvents_Source_DeliveryId",
+                table: "WebhookEvents");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "WebhookEvents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Payload",
+                table: "WebhookEvents",
+                type: "character varying(100000)",
+                maxLength: 100000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WebhookId",
+                table: "WebhookEvents",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "WebhookRoutes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Source = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebhookRoutes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebhookEvents_WebhookId_DeliveryId",
+                table: "WebhookEvents",
+                columns: new[] { "WebhookId", "DeliveryId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebhookRoutes_Source",
+                table: "WebhookRoutes",
+                column: "Source");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebhookRoutes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WebhookEvents_WebhookId_DeliveryId",
+                table: "WebhookEvents");
+
+            migrationBuilder.DropColumn(
+                name: "WebhookId",
+                table: "WebhookEvents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Payload",
+                table: "WebhookEvents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100000)",
+                oldMaxLength: 100000);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "WebhookEvents",
+                type: "integer",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebhookEvents_Source_DeliveryId",
+                table: "WebhookEvents",
+                columns: new[] { "Source", "DeliveryId" },
+                unique: true);
+        }
+    }
+}
