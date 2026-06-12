d8037a6c27088978e559f00f39dd2aeb9d1fca29
grahsp <pontus.grahsp@gmail.com>
2026-06-12 21:44:51 +0200

    chore: add migrations for new WebhookDestination and changes made

---
+++ backend/WebhookGateway.API/Persistence/Migrations/20260612202036_WebhookDestinations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebhookGateway.API.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WebhookDestinations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebhookDestinations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebhookRouteId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Url = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebhookDestinations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebhookDestinations_WebhookRoutes_WebhookRouteId",
+                        column: x => x.WebhookRouteId,
+                        principalTable: "WebhookRoutes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebhookDestinations_WebhookRouteId_Url",
+                table: "WebhookDestinations",
+                columns: new[] { "WebhookRouteId", "Url" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebhookDestinations");
+        }
+    }
+}
